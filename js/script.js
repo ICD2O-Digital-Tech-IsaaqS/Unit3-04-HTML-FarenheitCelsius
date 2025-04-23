367ae1748bfda11444e6c4e1090408ec8abe7568
Isaaq Simon <IsaaqS>
2025-04-23 11:53:42 +0000

    Farenheit to Celsius

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Isaaq Simon All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Isaaq Simon
+// Created on: April 2025
 // This file contains the JS functions for index.html
+function convertTemperature() {
+  const fahrenheit = parseFloat(document.getElementById('fahrenheit').value);
+  const resultElement = document.getElementById('result');
+  
+  const celsius = (fahrenheit - 32) * 5 / 9;
+  const message = isNaN(celsius)
+    ? "Please enter a valid number."
+    : `Temperature in Celsius: ${celsius.toFixed(2)}°C`;
+  
+  resultElement.innerText = message;
+}
